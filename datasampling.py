9dcc6fae68612697f9552d757a909a215cae0661
Linh Ha <racc0909@gmail.com>
2024-10-20 09:46:06 +0200

    add more datacleaning to crime and weather. add category to crime

--- datasampling.py
+++ datasampling.py
@@ -2,14 +2,37 @@
 
 # Import packages
 import pandas as pd
+from crime_categories import crime_categories
 
 # Read the full data
 df = pd.read_csv('data/la_crime_2020_2024_full.csv', sep=",")
 
-
 ### REDUCE DATA
 # Randomly sample 200000 observations from the original data
 df_sample = df.sample(n=200000, random_state=2)
+
+# Drop unnecessary columns and data
+df_sample = df_sample.drop(columns=['AREA','Rpt Dist No', 'Part 1-2', 'Crm Cd','Mocodes','Premis Cd','Weapon Used Cd','Status', 'Cross Street'])
+df_sample = df_sample[df_sample['Date Rptd'].str.contains('2024') == False]
+
+# Insert crime categories
+# Function to categorize crimes based on the crime categories dictionary
+def categorize_crime(crime):
+    for category, crimes in crime_categories.items():
+        if crime in crimes:
+            return category
+    return 'OTHER'  # For crimes that don't fall into the defined categories
+
+# Apply the function to each row in the dataset and create a new column 'crime category'
+df_sample['Category'] = df_sample['Crm Cd Desc'].apply(categorize_crime)
+
+# Reorder the columns
+new_order = [
+    'DR_NO', 'Date Rptd', 'DATE OCC', 'TIME OCC', 'AREA NAME', 'Crm Cd Desc', 'Category',
+    'Vict Age', 'Vict Sex', 'Vict Descent', 'Premis Desc', 'Weapon Desc', 'Status Desc',
+    'Crm Cd 1', 'Crm Cd 2', 'Crm Cd 3', 'Crm Cd 4', 'LOCATION', 'LAT', 'LON'
+]
+df_sample = df_sample[new_order]
 
 # Export the sampled data into a csv file
 df_sample.to_csv('data/la_crime_2020_2024_reduced.csv', index=False)  
@@ -20,7 +43,7 @@ df_safe = df_sample[(df_sample["Vict Age"] > 18) | (df_sample["Vict Age"] < 0)]
 
 ## Remove all cases that contains sensitive crime description
 # Words to check for
-words = ['child', 'sex', 'rape']
+words = ['child', 'sex', 'rape', 'chld', 'animal', 'philia']
 
 # Use str.contains with a regular expression to match any of the words
 pattern = '|'.join(words)  # Create a pattern with | as the separator
--- weatherdata_cleaning.py
+++ weatherdata_cleaning.py
@@ -2,17 +2,16 @@ import pandas as pd
 
 df = pd.read_csv('data/la_weather_2020_2024_original.csv', sep=',')
 
+keep_columns = ['datetime', 'temp', 'feelslike', 'humidity', 'windspeed']
+
 # Drop unwanted columns
-df_new = df.drop(columns=['name', 'precip','precipprob','precipcover','preciptype','sealevelpressure','severerisk','stations'])
+df_new = df[keep_columns]
 
 df_new['datetime'] = pd.to_datetime(df_new['datetime'], format='%Y-%m-%d')
-df_new['sunrise'] = pd.to_datetime(df_new['sunrise'], format="%Y-%m-%dT%H:%M:%S")
-df_new['sunset'] = pd.to_datetime(df_new['sunset'], format="%Y-%m-%dT%H:%M:%S")
-
+#df_new['sunrise'] = pd.to_datetime(df_new['sunrise'], format="%Y-%m-%dT%H:%M:%S")
+#df_new['sunset'] = pd.to_datetime(df_new['sunset'], format="%Y-%m-%dT%H:%M:%S")
 
 df_new.info()
-
-
 
 # Export cleaned data as a new csv
 df_new.to_csv('data/la_weather_2020_2024_cleaned.csv', index=False)
